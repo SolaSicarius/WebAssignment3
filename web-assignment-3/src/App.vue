e3ad15fcb9c6fd9ae9ef7563f2a5c6f1e3263837
SolaSicarius <20347376@studentmail.ucol.ac.nz>
2023-06-13 21:31:43 +1200

    Creating Sign Up Page

--- web-assignment-3/src/App.vue
+++ web-assignment-3/src/App.vue
@@ -1,11 +1,21 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
+  <img class="logo" alt="MovieBox logo" src="./assets/MovieBox_Logo_No_Background.png">
+  <SignUp />
+  <div class="register">
+    <input type="text" placeholder="Enter Name" />
+    <input type="text" placeholder="Enter Email" />
+    <input type="password" placeholder="Enter Password" />
+  <button>Sign Up</button>
+</div>
 </template>
 
 <script>
-
+import SignUp from './components/SignUp.vue'
 export default {
   name: 'App',
+  components:{
+    SignUp
+  }
 }
 </script>
 
@@ -17,5 +27,28 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.logo{
+  width:100px;
+}
+
+.register input{
+  width: 300px;
+  height: 40px;
+  padding-left: 20px;
+  display: block;
+  margin-left: auto;
+  margin-bottom: 30px;
+  margin-right: auto;
+  border: 1px solid lightgray;
+}
+
+.register button{
+  width: 320px;
+  height: 40px;
+  border: 1px solid lightgray;
+  background: lightgray;
+  cursor: pointer;
 }
 </style>
---
+++ web-assignment-3/src/components/SignUp.vue
@@ -0,0 +1,8 @@
+<template>
+<h1>Sign Up</h1>
+</template>
+<script>
+export default {
+        name :'SignUp'
+}
+</script>
