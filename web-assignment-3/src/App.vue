59e41eef2c44aeea4e1df80959a93acc1b29900d
SolaSicarius <20347376@studentmail.ucol.ac.nz>
2023-06-14 22:06:01 +1200

    Getting user sign up working

--- web-assignment-3/src/App.vue
+++ web-assignment-3/src/App.vue
@@ -5,25 +5,48 @@
       <img style="width: 100px" alt="MovieBox logo" src="./assets/MovieBox_Logo_No_Background.png">
     </div>
   </header>
-  <SignUp />
+
   <div class="container">
     <div class="columns is-centered">
       <div class="column has-text-centered is-6-fullhd is-6-desktop is-6-tablet is-12-mobile">
-        <input class="input is-primary is-normal" type="text" placeholder="Enter Name" />
-        <input class="input is-primary is-normal" type="text" placeholder="Enter Email" />
-        <input class="input is-primary is-normal" type="password" placeholder="Enter Password" />
-        <button class="button is-primary is-fullwidth is-normal">Sign Up</button>
+        <input class="input is-primary is-normal" type="text" v-model="name" placeholder="Enter Name" />
+        <input class="input is-primary is-normal" type="text" v-model="email" placeholder="Enter Email" />
+        <input class="input is-primary is-normal" type="password" v-model="password" placeholder="Enter Password" />
+        <button class="button is-primary is-fullwidth is-normal" v-on:click="signUp">Sign Up</button>
       </div>
     </div>
 </div>
 </template>
 
 <script>
-import SignUp from './components/SignUp.vue'
+import axios from 'axios'
 export default {
-  name: 'App',
-  components:{
-    SignUp
+  name: 'SignUp',
+  data()
+  {
+    return {
+      name: '',
+      email: '',
+      password: ''
+    }
+  },
+  methods:{
+    async signUp()
+    {
+      //console.warn("signup",this.name,this.email,this.password)
+      let result = await axios.post("http://localhost:3000/users",{
+        name:this.name,
+        email:this.email,
+        password:this.password
+      })
+
+      console.warn(result);
+      if(result.status==201)
+      {
+        alert("sign up complete");
+        localStorage.setItem("user-info",JSON.stringify(result.data))
+      }
+    }
   }
 }
 </script>
