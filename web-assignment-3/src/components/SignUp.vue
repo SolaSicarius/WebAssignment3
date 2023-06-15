accf778f61b17f333b15a7707126557d00c1d56c
SolaSicarius <20347376@studentmail.ucol.ac.nz>
2023-06-15 21:17:43 +1200

    User now needs to sign up to see home page

--- web-assignment-3/src/components/SignUp.vue
+++ web-assignment-3/src/components/SignUp.vue
@@ -44,6 +44,13 @@ export default {
         this.$router.push({name: 'HomePage'})
       }
     }
-  }
+  },
+  mounted() {
+        let user = localStorage.getItem('user-info');
+        if(user)
+        {
+                this.$router.push({name: 'HomePage'})
+        }
+  },
 }
 </script>
