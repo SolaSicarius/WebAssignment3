4b7145cb0aa5b1ca0db74aa61b525cb65e2e8cbb
SolaSicarius <20347376@studentmail.ucol.ac.nz>
2023-06-15 21:27:57 +1200

    Creating Login Page

---
+++ web-assignment-3/src/components/Login.vue
@@ -0,0 +1,19 @@
+<template>
+<h1>Login Page</h1>
+<div class="container">
+        <div class="columns is-centered">
+                <div class="column has-text-centered is-6-fullhd is-6-desktop is-6-tablet is-12-mobile">
+                <input class="input is-primary is-normal" type="text" v-model="email" placeholder="Enter Email" />
+                <input class="input is-primary is-normal" type="password" v-model="password" placeholder="Enter Password" />
+                <button class="button is-primary is-fullwidth is-normal" v-on:click="logIn">Login</button>
+                <router-link to="/sign-up">Sign Up</router-link>
+                </div>
+        </div>
+</div>
+</template>
+
+<script>
+export default {
+        name: "LogIn"
+}
+</script>
--- web-assignment-3/src/components/SignUp.vue
+++ web-assignment-3/src/components/SignUp.vue
@@ -1,4 +1,5 @@
 <template>
+<h1>Sign Up Page</h1>
 <div class="container">
         <div class="columns is-centered">
                 <div class="column has-text-centered is-6-fullhd is-6-desktop is-6-tablet is-12-mobile">
@@ -6,6 +7,7 @@
                 <input class="input is-primary is-normal" type="text" v-model="email" placeholder="Enter Email" />
                 <input class="input is-primary is-normal" type="password" v-model="password" placeholder="Enter Password" />
                 <button class="button is-primary is-fullwidth is-normal" v-on:click="signUp">Sign Up</button>
+                <router-link to="/login">Login</router-link>
                 </div>
         </div>
 </div>
