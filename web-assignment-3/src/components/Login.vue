05116a4babe6710e2fb4459bfa65eab06c472b14
SolaSicarius <20347376@studentmail.ucol.ac.nz>
2023-06-15 22:03:32 +1200

    login integration

--- web-assignment-3/src/components/Login.vue
+++ web-assignment-3/src/components/Login.vue
@@ -5,7 +5,7 @@
                 <div class="column has-text-centered is-6-fullhd is-6-desktop is-6-tablet is-12-mobile">
                 <input class="input is-primary is-normal" type="text" v-model="email" placeholder="Enter Email" />
                 <input class="input is-primary is-normal" type="password" v-model="password" placeholder="Enter Password" />
-                <button class="button is-primary is-fullwidth is-normal" v-on:click="logIn">Login</button>
+                <button class="button is-primary is-fullwidth is-normal" v-on:click="login">Login</button>
                 <router-link to="/sign-up">Sign Up</router-link>
                 </div>
         </div>
@@ -13,7 +13,41 @@
 </template>
 
 <script>
+import axios from 'axios'
 export default {
-        name: "LogIn"
+        name: "LogIn",
+        data()
+        {
+                return {
+                        email: '',
+                        password: ''
+                }
+                
+
+        },
+        methods: 
+        {
+                async login()
+                {
+                        let result = await axios.get(
+                                `http://localhost:3000/users?email=${this.email}&password=${this.password}`
+                        )
+
+                        if(result.status==200 && result.data.length>0)
+                        {
+                                localStorage.setItem("user-info",JSON.stringify(result.data[0]))
+                                this.$router.push({name: 'HomePage'})
+                        }
+                        console.warn(result)
+                }
+        },
+        mounted() {
+        let user = localStorage.getItem('user-info');
+        if(user)
+                {
+                        this.$router.push({name: 'HomePage'})
+                }
+        }
+        
 }
 </script>
