c9d8e2a0173d3fe2cc8f3dc6a0236eaf96e6153b
SolaSicarius <20347376@studentmail.ucol.ac.nz>
2023-06-17 23:00:01 +1200

    Made movie table fit on screen better

--- web-assignment-3/src/components/Home.vue
+++ web-assignment-3/src/components/Home.vue
@@ -3,33 +3,41 @@
   <br />
   <h1>Hello {{ name }}, Welcome to the Home Page</h1>
   <br />
-  <table class="table">
-    <thead>
-      <tr>
-        <td>Movie Title</td>
-        <td>Description</td>
-        <td>Release Date</td>
-        <td>Actions</td>
-      </tr>
-    </thead>
-    <tbody>
-      <tr v-for="item in movies" :key="item.id">
-        <td>{{ item.title }}</td>
-        <td>{{ item.description }}</td>
-        <td>{{ item.release }}</td>
-        <td>
-          <router-link :to="'/update-movie/' + item.id">
-            <button>
-              <strong>Update Details</strong>
-            </button>
-          </router-link>
-          <button v-on:click="deleteMovie(item.id)">
-            <strong>Delete Movie</strong>
-          </button>
-        </td>
-      </tr>
-    </tbody>
-  </table>
+  <div class="container">
+    <div class="columns is-centered">
+      <div
+        class="column has-text-centered is-12-fullhd is-12-desktop is-12-tablet is-12-mobile"
+      >
+        <table class="table">
+          <thead>
+            <tr>
+              <td>Movie Title</td>
+              <td>Description</td>
+              <td>Release Date</td>
+              <td>Actions</td>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="item in movies" :key="item.id">
+              <td>{{ item.title }}</td>
+              <td>{{ item.description }}</td>
+              <td>{{ item.release }}</td>
+              <td>
+                <router-link :to="'/update-movie/' + item.id">
+                  <button>
+                    <strong>Update Details</strong>
+                  </button>
+                </router-link>
+                <button v-on:click="deleteMovie(item.id)">
+                  <strong>Delete Movie</strong>
+                </button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -47,17 +55,14 @@ export default {
     Header,
   },
   methods: {
-    async deleteMovie(id)
-    {
+    async deleteMovie(id) {
       //console.warn(id)
-      let result = await axios.delete("http://localhost:3000/movies/" + id)
-      if(result.status==200)
-      {
+      let result = await axios.delete("http://localhost:3000/movies/" + id);
+      if (result.status == 200) {
         this.loadData();
       }
     },
-    async loadData()
-    {
+    async loadData() {
       let user = localStorage.getItem("user-info");
       this.name = JSON.parse(user).name;
       if (!user) {
@@ -65,7 +70,7 @@ export default {
       }
       let result = await axios.get("http://localhost:3000/movies");
       this.movies = result.data;
-    }
+    },
   },
   async mounted() {
     this.loadData();
